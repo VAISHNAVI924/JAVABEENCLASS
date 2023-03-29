b6079ddeac3ee05a525be8ef65dd12314c6d3c3d
Vaishnavi Narasing Hotkar <96836003+VAISHNAVI924@users.noreply.github.com>
2023-03-29 16:26:20 +0530

    ECLIPSE

---
+++ Employee.java
@@ -0,0 +1,20 @@
+package javabeanclass;
+
+public class Employee {
+	private  String name;
+	private int id;
+	public void SetName(String name) {
+		this.name=name;
+	}
+public void SetId(int id) {
+	this.id=id;
+	
+}
+public String getName() {
+	return name;
+	
+}
+public int getId() {
+	return id;
+}
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+package javabeanclass;
+
+public class Main {
+
+	public static void main(String[] args) {
+	Employee emp=new Employee();
+	emp.SetName("kavya");
+	emp.SetId(2435);
+	System.out.println(emp.getId());
+	System.out.println(emp.getName());
+
+	}
+
+}
